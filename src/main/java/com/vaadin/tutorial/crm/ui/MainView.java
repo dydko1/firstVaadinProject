d7231eef7688595b122b33f69fbe54977c15fa36
dydko1 <miroslaw.dyduch@gmail.com>
2021-06-26 08:33:59 +0200

    temporary

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -6,18 +6,39 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    private ContactService contactService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
 
+    public MainView() {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 }
