5d828165ba42c40c3c03eb094175e7e4da979ce5
dydko1 <miroslaw.dyduch@gmail.com>
2021-07-29 06:22:51 +0200

    local commit

--- src/main/java/com/vaadin/tutorial/crm/views/ChangeHandler.java
+++
@@ -1,5 +0,0 @@
-package com.vaadin.tutorial.crm.views;
-
-public interface ChangeHandler {
-    void OnChange();
-}
--- src/main/java/com/vaadin/tutorial/crm/views/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/views/MainView.java
@@ -1,66 +1,31 @@
 package com.vaadin.tutorial.crm.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import com.vaadin.tutorial.crm.model.Customer;
-import com.vaadin.tutorial.crm.repository.CustomerRepository;
-import org.springframework.util.StringUtils;
-
+import com.vaadin.flow.server.PWA;
 
 @Route
+@PWA(name = "Vaadin Chat", shortName = "Vaadin Chat")
+@CssImport("frontend://styles/styles.css")
+@Push
 public class MainView extends VerticalLayout {
+    public MainView() {
+        setSizeFull();
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        addClassName("main-view");
 
-    private final CustomerRepository repo;
-    private final CustomerEditor editor;
-    final Grid<Customer> grid;
-    final TextField filter;
-    private final Button addNewBtn;
+        H1 header = new H1("Vaadin Chat");
+        header.getElement().getThemeList().add("dark");
 
-    public MainView(CustomerRepository repo, CustomerEditor editor) {
-        this.repo = repo;
-        this.editor = editor;
-        this.grid = new Grid<>(Customer.class);
-        this.filter = new TextField();
-        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
-
-        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-        add(actions, grid, editor);
-
-        grid.setHeight("300px");
-        grid.setColumns("id", "firstName", "lastName");
-        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-
-        filter.setPlaceholder("Filter by last name....");
-
-        filter.setValueChangeMode(ValueChangeMode.EAGER);
-        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
-
-        grid.asSingleSelect().addValueChangeListener(e -> {
-            editor.editCustomer(e.getValue());
-        });
-
-        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
-
-        editor.setChangeHandler(() -> {
-            editor.setVisible(false);
-            listCustomers(filter.getValue());
-        });
-
-        listCustomers(null);
-    }
-
-    // tag::listCustomers[]
-    void listCustomers(String filterText) {
-        if (StringUtils.isEmpty(filterText)) {
-            grid.setItems(repo.findAll());
-        } else {
-            grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
-        }
+        add(header);
     }
 }
