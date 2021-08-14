c74a825631e69f8d925f8f26e8c76635451ef8fb
dydko1 <miroslaw.dyduch@gmail.com>
2021-08-14 09:40:48 +0200

    temp1

--- src/main/java/com/vaadin/tutorial/crm/views/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/views/MainView.java
@@ -1,6 +1,30 @@
 package com.vaadin.tutorial.crm.views;
 
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 
+@Route
 public class MainView extends VerticalLayout {
+
+    public MainView() {
+        VerticalLayout todoList = new VerticalLayout();
+        TextField textField = new TextField();
+        Button buttonAdd = new Button("Add");
+
+        buttonAdd.addClickShortcut(Key.ENTER);
+        buttonAdd.addClickListener(click -> {
+            Checkbox checkboxAdd = new Checkbox(textField.getValue());
+            todoList.add(checkboxAdd);
+            System.out.println("Test !!!");
+        });
+
+        add(new H1("Vaadin todo"), todoList,
+                new HorizontalLayout(textField, buttonAdd));
+    }
 }
