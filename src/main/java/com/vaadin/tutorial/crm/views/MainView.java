8cb87f5c539ad81dd065fa61a53ea456630e8a8b
dydko1 <miroslaw.dyduch@gmail.com>
2021-08-15 07:21:14 +0200

    lesson components finished

--- src/main/java/com/vaadin/tutorial/crm/views/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/views/MainView.java
@@ -1,8 +1,10 @@
 package com.vaadin.tutorial.crm.views;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -16,6 +18,10 @@ public class MainView extends VerticalLayout {
         VerticalLayout todoList = new VerticalLayout();
         TextField textField = new TextField();
         Button buttonAdd = new Button("Add");
+        Button button2 = new Button("I'm a button");
+
+        HorizontalLayout layout = new HorizontalLayout(button2, new DatePicker("Pick a date"));
+        layout.setDefaultVerticalComponentAlignment(Alignment.END);
 
         buttonAdd.addClickShortcut(Key.ENTER);
         buttonAdd.addClickListener(click -> {
@@ -23,8 +29,10 @@ public class MainView extends VerticalLayout {
             todoList.add(checkboxAdd);
             System.out.println("Test !!!");
         });
+        button2.addClickListener(clickEvent ->
+                add(new Text("Clicked!")));
 
         add(new H1("Vaadin todo"), todoList,
-                new HorizontalLayout(textField, buttonAdd));
+                new HorizontalLayout(textField, buttonAdd,button2),layout);
     }
 }
