2beec78b75256e4d87d25a69349eee5c9a29b274
dydko1 <miroslaw.dyduch@gmail.com>
2021-08-16 20:58:27 +0200

    chapter 003 finished

--- src/main/java/com/vaadin/tutorial/crm/backend/repository/CompanyRepository.java
+++ src/main/java/com/vaadin/tutorial/crm/backend/repository/CompanyRepository.java
@@ -1,7 +1,8 @@
 package com.vaadin.tutorial.crm.backend.repository;
 
 import com.vaadin.tutorial.crm.backend.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CompanyRepository extends CrudRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
