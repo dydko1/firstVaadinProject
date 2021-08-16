2e1fb3ce6ae269453c77e05252cbe2d4ed6b53bd
dydko1 <miroslaw.dyduch@gmail.com>
2021-08-16 22:12:34 +0200

    tmp

--- src/main/java/com/vaadin/tutorial/crm/views/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/views/MainView.java
@@ -5,34 +5,36 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
-@Route
+@Route("")
 public class MainView extends VerticalLayout {
 
+    private ContactService contractService;
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+
     public MainView() {
-        VerticalLayout todoList = new VerticalLayout();
-        TextField textField = new TextField();
-        Button buttonAdd = new Button("Add");
-        Button button2 = new Button("I'm a button");
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+        updateList();
+    }
 
-        HorizontalLayout layout = new HorizontalLayout(button2, new DatePicker("Pick a date"));
-        layout.setDefaultVerticalComponentAlignment(Alignment.END);
+    private void configureGrid() {
+        grid.addClassName("contract-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
+    }
 
-        buttonAdd.addClickShortcut(Key.ENTER);
-        buttonAdd.addClickListener(click -> {
-            Checkbox checkboxAdd = new Checkbox(textField.getValue());
-            todoList.add(checkboxAdd);
-            System.out.println("Test !!!");
-        });
-        button2.addClickListener(clickEvent ->
-                add(new Text("Clicked!")));
-
-        add(new H1("Vaadin todo"), todoList,
-                new HorizontalLayout(textField, buttonAdd,button2),layout);
+    private void updateList() {
+        grid.setItems(contractService.findAll());
     }
 }
