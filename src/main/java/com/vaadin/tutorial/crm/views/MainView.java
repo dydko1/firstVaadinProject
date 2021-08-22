2e755248b87f39d7a2bb7e53a7dcabf0d9ca773d
dydko1 <miroslaw.dyduch@gmail.com>
2021-08-22 07:35:17 +0200

    chaper 06 finished

--- src/main/java/com/vaadin/tutorial/crm/views/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/views/MainView.java
@@ -5,7 +5,9 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -17,12 +19,14 @@ import com.vaadin.tutorial.crm.backend.entity.Contact;
 import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
+@CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
     private ContactService contractService;
 
     private Grid<Contact> grid = new Grid<>(Contact.class);
     private TextField filterText = new TextField();
+    private ContactForm form;
 
     public MainView(ContactService contactService) {
         this.contractService = contactService;
@@ -30,7 +34,13 @@ public class MainView extends VerticalLayout {
         setSizeFull();
         configureGrid();
         configureFilter();
-        add(filterText, grid);
+        form = new ContactForm();
+        Div content = new Div(grid, form);
+        content.addClassName("content");
+        content.setId("contencik");
+        content.setSizeFull();
+
+        add(filterText, content);
         updateList();
     }
 
